8c508b1799de1e8d62e60323a3a305787efec07a
crabman77 <crabman77.github@gmail.com>
2015-10-06 03:20:24 +0200

    refactor code and add nodes

--- mods/default/nodes.lua
+++ mods/default/nodes.lua
@@ -13,75 +13,67 @@
   3.
 	default:dirt_with_grass
 
+  4.
+	default:sand
+	default:
+	default:
+
+  5.
+	default:tiled_brick_monochrome
+	default:tiled_brick_blue
+	default:tiled_brick_cyan
+	default:tiled_brick_grey
+	default:tiled_brick_light_blue
+
+  6.
+	default:quarter_green
+	default:quarter_red
+	default:quarter_cyan
+	default:quarter_white
+
+  7.
+	default:
+	default:
+	default:
+
+  8.
+	default:
+	default:
+
+  9. Pipes
+	default:
+	default:
+	default:
+	default:
+
+  10.
+
+  11.
+	default:
+	default:
+
+  12.
+	default:
+	default:
+
+  13.
+	default:spikes
+
+  14.  
+	default:ice
+
+  15.
+	default:cloud
+
+  16.
+	default:climbing_plant
+
+  17.
+	default:coin
+
 ]]--
 
 -- 1. Brick, cobble, tiles
-minetest.register_node("default:brick", {
-	description = "Bricks",
-	tiles = {"default_brick.png"},
-	groups = {unbreakable = 1},
-	sounds = default.node_sound_stone_defaults(),
-})
-
-minetest.register_node("default:cobble", {
-	description = "Cobblestone",
-	tiles = {"default_cobble.png"},
-	groups = {unbreakable = 1},
-	sounds = default.node_sound_stone_defaults(),
-})
-
-minetest.register_node("default:tiles", {
-	description = "Tiles",
-	tiles = {"default_tiles.png"},
-	groups = {unbreakable = 1},
-	sounds = default.node_sound_defaults(),
-})
-
--- 2. Stipes, stipes block with surface
-minetest.register_node("default:stripes", {
-	description = "Stripes",
-	tiles = {"default_stripes.png"},
-	groups = {unbreakable = 1},
-	sounds = default.node_sound_defaults(),
-})
-
-minetest.register_node("default:stripes", {
-	description = "Stripes Surface",
-	tiles = {"default_stripe_top.png", "default_stripes.png",
-		"default_stripes.png^default_surface_layer.png"}
-	groups = {unbreakable = 1},
-	sounds = default.node_sound_defaults(),
-})
-
--- 3. Dirt with Grass
-minetest.register_node("default:dirt_with_grass",
-	description = "Dirt with Grass",
-        tiles = {"default_grass.png", "default_dirt.png",
-                {name = "default_dirt.png^default_grass_side.png",
-                        tileable_vertical = false}},
-        groups = {crumbly = 3, soil = 1},
-        drop = 'default:dirt',
-        sounds = default.node_sound_dirt_defaults({
-                footstep = {name = "default_grass_footstep", gain = 0.25},
-        }),
-})
-
-	
-
-
--- Unordered nodes
---stone
-minetest.register_node("default:stone", {
-	description = "Stone",
-	tiles = {"default_stone.png"},
-	is_ground_content = true,
-	groups = {unbreakable=1},
-	drop = {},
-	sounds = default.node_sound_stone_defaults(),
-})
-
-
-
 --brick
 minetest.register_node("default:brick_grey", {
 	description = "Brick Block Grey Unbreakable",
@@ -107,6 +99,14 @@ minetest.register_node("default:brick_sand", {
 	sounds = default.node_sound_stone_defaults(),
 })
 
+--Cobble
+minetest.register_node("default:cobble", {
+	description = "Cobblestone",
+	tiles = {"default_cobble.png"},
+	groups = {unbreakable = 1},
+	sounds = default.node_sound_stone_defaults(),
+})
+
 --Tiles
 minetest.register_node("default:tiles_orange", {
 	description = "Brick Block Tiles Orange",
@@ -118,136 +118,46 @@ minetest.register_node("default:tiles_orange", {
 })
 
 
---breakable block
-minetest.register_node("default:breakable_brick_orange", {
-	description = "Brick Block Orange",
-	tiles = {"default_breakable_brick_orange.png"},
-	is_ground_content = false,
-	drop = "default:piece",
-	groups = {unbreakable=1},
-	sounds = default.node_sound_stone_defaults(),
-	on_punch = function(pos, node, puncher, pointed_thing)
-		local meta = minetest.get_meta(pos)
-		local nb = meta:get_int("piece_nb")
-		minetest.sound_play("block_break", {pos = pos, gain = 0.3, max_hear_distance = 6})--FIXME add a "block_break.ogg" sound
-		-- FIXME add particules
-		
-		
-		minetest.set_node(pos, {name="air"})
-	end,
-})
-
-minetest.register_node("default:breakable_brick_white", {
-	description = "Brick Block White",
-	tiles = {"default_breakable_brick_white.png"},
-	is_ground_content = false,
-	drop = "default:piece",
-	groups = {unbreakable=1},
-	sounds = default.node_sound_stone_defaults(),
-})
-
-minetest.register_node("default:unbreakable_brick_or", {
-	description = "Brick Block Or Unbreakable",
-	tiles = {"default_unbreakable_brick_or.png"},
-	is_ground_content = false,
+-- 2. Stripes, stripes block with surface
+--Stripes
+minetest.register_node("default:stripes", {
+	description = "Stripes",
+	tiles = {"default_stripes.png"},
 	drop = {},
 	groups = {unbreakable=1},
 	sounds = default.node_sound_stone_defaults(),
 })
 
-minetest.register_node("default:random_drop", {
-	description = "Random Drop Pieces (Unbreakable)",
-	tiles = {"default_unbreakable_brick_or.png"},
-	is_ground_content = false,
-	drop = {},
-	groups = {unbreakable=1},
-	sounds = default.node_sound_stone_defaults(),
-	paramtype = "light",
-	light_source = 13,
-	inventory_image = minetest.inventorycube("default_surprise_node.png"),
-	tiles = {
-		"default_surprise_node_top.png", "default_surprise_node_top.png", {name = "default_surprise_node_animated.png", animation={type = "vertical_frames", aspect_w= 16, aspect_h = 16, length = 1.5}}
+minetest.register_node("default:stripes_surface", {
+	description = "Stripes Surface",
+	tiles = {"default_stripes.png", "default_stripes.png",
+			"default_stripes_surface.png", "default_stripes_surface.png",
+			"default_stripes_surface.png", "default_stripes_surface.png",
 	},
-	special_tiles = {
-		{
-			image = "default_surprise_node_top.png",
-			backface_culling=false,
-			animation={type = "vertical_frames", aspect_w= 16, aspect_h = 16, length = 0.6}
-		},
-		{
-			image = "default_surprise_node_top.png",
-			backface_culling=true,
-			animation={type = "vertical_frames", aspect_w= 16, aspect_h = 16, length = 0.6}
-		}
+	paramtype2 = "facedir",
+	is_ground_content = false,
+	drop = {},
+	groups = {unbreakable=1},
+	sounds = default.node_sound_stone_defaults(),
+})
+
+
+-- 3. Dirt with Grass
+minetest.register_node("default:dirt_with_grass", {
+	description = "Dirt With Grass",
+	tiles = {"default_grass.png", "default_dirt.png",
+		{name = "default_dirt.png^default_grass_side.png",
+			tileable_vertical = false}
 	},
-	on_punch = function(pos, node, puncher, pointed_thing)
-		local meta = minetest.get_meta(pos)
-		local nb = meta:get_int("piece_nb")
-		minetest.sound_play("drop_piece", {pos = pos, gain = 0.3, max_hear_distance = 6})--FIXME add a "drop_piece.ogg" sound
-		-- FIXME add piece entity or particules
-		
-		
-		if nb and nb > 1 then
-			meta:set_int("piece_nb", nb-1)
-		else
-			minetest.set_node(pos, {name= "default:unbreakable_brick_or"})
-		end
-
-	end,
-	on_construct = function(pos)
-		local rand = math.random(1, 10)
-		local nb
-		if rand == 1 then
-			nb = 10
-		elseif rand <=3  then
-			nb = 5
-		else
-			nb = 1
-		end
-		local meta = minetest.get_meta(pos)
-		meta:set_int("piece_nb", nb)
-	end,
-})
-
--- Quarter
-minetest.register_node("default:quarter_cyan", {
-	description = "Quarter Cyan",
-	tiles = {"default_quarter_cyan.png"},
-	is_ground_content = false,
-	drop = {},
 	groups = {unbreakable=1},
-	sounds = default.node_sound_stone_defaults(),
-})
-
-minetest.register_node("default:quarter_green", {
-	description = "Quarter Green",
-	tiles = {"default_quarter_green.png"},
-	is_ground_content = false,
 	drop = {},
-	groups = {unbreakable=1},
-	sounds = default.node_sound_stone_defaults(),
-})
-
-minetest.register_node("default:quarter_red", {
-	description = "Quarter Red",
-	tiles = {"default_quarter_red.png"},
-	is_ground_content = false,
-	drop = {},
-	groups = {unbreakable=1},
-	sounds = default.node_sound_stone_defaults(),
-})
-
-minetest.register_node("default:quarter_white", {
-	description = "Quarter White",
-	tiles = {"default_quarter_white.png"},
-	is_ground_content = false,
-	drop = {},
-	groups = {unbreakable=1},
-	sounds = default.node_sound_stone_defaults(),
+	sounds = default.node_sound_dirt_defaults({
+		footstep = {name="default_grass_footstep", gain=0.8},
+	}),
 })
 
 
---Sand
+-- 4. Sand
 minetest.register_node("default:sand", {
 	description = "Sand Unbreakable",
 	tiles = {"default_sand.png"},
@@ -257,55 +167,7 @@ minetest.register_node("default:sand", {
 })
 
 
-minetest.register_node("default:dirt_with_grass", {
-	description = "Dirt with Grass",
-	tiles = {"default_grass.png", "default_dirt.png",
-		{name = "default_dirt.png^default_grass_side.png",
-			tileable_vertical = false}},
-	groups = {unbreakable=1},
-	drop = {},
-	sounds = default.node_sound_dirt_defaults({
-		footstep = {name="default_grass_footstep", gain=0.8},
-	}),
-})
-
-minetest.register_node("default:damier_black", {
-	description = "Black Damier Block",
-	tiles = {"default_damier_black.png"},
-	is_ground_content = false,
-	drop = {},
-	groups = {unbreakable=1},
-	sounds = default.node_sound_stone_defaults(),
-})
-
-minetest.register_node("default:damier_white", {
-	description = "White Damier Block",
-	tiles = {"default_damier_white.png"},
-	is_ground_content = false,
-	drop = {},
-	groups = {unbreakable=1},
-	sounds = default.node_sound_stone_defaults(),
-})
-
---Stripes
-minetest.register_node("default:stripes", {
-	description = "Stripes",
-	tiles = {"default_stripes.png"},
-	is_ground_content = false,
-	drop = {},
-	groups = {unbreakable=1},
-	sounds = default.node_sound_stone_defaults(),
-})
-
-minetest.register_node("default:stripes_surface", {
-	description = "Stripes Surface",
-	tiles = {"default_stripes_surface.png"},
-	is_ground_content = false,
-	drop = {},
-	groups = {unbreakable=1},
-	sounds = default.node_sound_stone_defaults(),
-})
-
+--5. Tiled Brick
 --tiled_brick
 minetest.register_node("default:tiled_brick_blue", {
 	description = "Tiled Brick Blue",
@@ -352,6 +214,205 @@ minetest.register_node("default:tiled_brick_monochrome", {
 	sounds = default.node_sound_stone_defaults(),
 })
 
+
+--6. Quarter
+-- Quarter
+minetest.register_node("default:quarter_cyan", {
+	description = "Quarter Cyan",
+	tiles = {"default_quarter_cyan.png"},
+	is_ground_content = false,
+	drop = {},
+	groups = {unbreakable=1},
+	sounds = default.node_sound_stone_defaults(),
+})
+
+minetest.register_node("default:quarter_green", {
+	description = "Quarter Green",
+	tiles = {"default_quarter_green.png"},
+	is_ground_content = false,
+	drop = {},
+	groups = {unbreakable=1},
+	sounds = default.node_sound_stone_defaults(),
+})
+
+minetest.register_node("default:quarter_red", {
+	description = "Quarter Red",
+	tiles = {"default_quarter_red.png"},
+	is_ground_content = false,
+	drop = {},
+	groups = {unbreakable=1},
+	sounds = default.node_sound_stone_defaults(),
+})
+
+minetest.register_node("default:quarter_white", {
+	description = "Quarter White",
+	tiles = {"default_quarter_white.png"},
+	is_ground_content = false,
+	drop = {},
+	groups = {unbreakable=1},
+	sounds = default.node_sound_stone_defaults(),
+})
+
+
+--7.
+--breakable block
+minetest.register_node("default:breakable_brick_orange", {
+	description = "Brick Block Orange",
+	tiles = {"default_breakable_brick_orange.png"},
+	is_ground_content = false,
+	drop = "default:piece",
+	groups = {unbreakable=1},
+	sounds = default.node_sound_stone_defaults(),
+	on_punch = function(pos, node, puncher, pointed_thing)
+		local meta = minetest.get_meta(pos)
+		local nb = meta:get_int("piece_nb")
+		minetest.sound_play("block_break", {pos = pos, gain = 0.3, max_hear_distance = 6})--FIXME add a "block_break.ogg" sound
+		-- FIXME add particules
+		
+		
+		minetest.set_node(pos, {name="air"})
+	end,
+})
+
+minetest.register_node("default:breakable_brick_white", {
+	description = "Brick Block White",
+	tiles = {"default_breakable_brick_white.png"},
+	is_ground_content = false,
+	drop = "default:piece",
+	groups = {unbreakable=1},
+	sounds = default.node_sound_stone_defaults(),
+})
+
+minetest.register_node("default:unbreakable_brick_or", {
+	description = "Brick Block Or Unbreakable",
+	tiles = {"default_unbreakable_brick_or.png"},
+	is_ground_content = false,
+	drop = {},
+	groups = {unbreakable=1},
+	sounds = default.node_sound_stone_defaults(),
+})
+
+
+--8.
+minetest.register_node("default:random_drop", {
+	description = "Random Drop Pieces (Unbreakable)",
+	tiles = {"default_unbreakable_brick_or.png"},
+	is_ground_content = false,
+	drop = {},
+	groups = {unbreakable=1},
+	sounds = default.node_sound_stone_defaults(),
+	paramtype = "light",
+	light_source = 13,
+	inventory_image = minetest.inventorycube("default_surprise_node.png"),
+	tiles = {
+		"default_surprise_node_top.png", "default_surprise_node_top.png", {name = "default_surprise_node_animated.png", animation={type = "vertical_frames", aspect_w= 16, aspect_h = 16, length = 1.5}}
+	},
+	special_tiles = {
+		{
+			image = "default_surprise_node_top.png",
+			backface_culling=false,
+			animation={type = "vertical_frames", aspect_w= 16, aspect_h = 16, length = 0.6}
+		},
+		{
+			image = "default_surprise_node_top.png",
+			backface_culling=true,
+			animation={type = "vertical_frames", aspect_w= 16, aspect_h = 16, length = 0.6}
+		}
+	},
+	on_punch = function(pos, node, puncher, pointed_thing)
+		local meta = minetest.get_meta(pos)
+		local nb = meta:get_int("piece_nb")
+		minetest.sound_play("drop_piece", {pos = pos, gain = 0.3, max_hear_distance = 6})--FIXME add a "drop_piece.ogg" sound
+		-- FIXME add piece entity or particules
+		
+		
+		if nb and nb > 1 then
+			meta:set_int("piece_nb", nb-1)
+		else
+			minetest.set_node(pos, {name= "default:random_drop_empty"})
+		end
+
+	end,
+	on_construct = function(pos)
+		local rand = math.random(1, 10)
+		local nb
+		if rand == 1 then
+			nb = 10
+		elseif rand <=3  then
+			nb = 5
+		else
+			nb = 1
+		end
+		local meta = minetest.get_meta(pos)
+		meta:set_int("piece_nb", nb)
+	end,
+})
+
+minetest.register_node("default:random_drop_empty", {
+	description = "Random Drop Empty",
+	tiles = {"default_surprise_node_top.png"},
+	drop = {},
+	groups = {unbreakable=1},
+	sounds = default.node_sound_stone_defaults(),
+})
+
+
+--9.
+
+--10.
+
+--11.
+
+--12.
+
+--13.
+
+--14.
+
+--15. Cloud
+minetest.register_node("default:cloud", {
+	description = "Cloud",
+	tiles = {"default_cloud.png"},
+	sounds = default.node_sound_defaults(),
+	groups = {not_in_creative_inventory=1},
+})
+
+--16.
+
+--17.
+
+
+-- Unordered nodes
+--stone
+minetest.register_node("default:stone", {
+	description = "Stone",
+	tiles = {"default_stone.png"},
+	is_ground_content = true,
+	groups = {unbreakable=1},
+	drop = {},
+	sounds = default.node_sound_stone_defaults(),
+})
+
+
+
+
+minetest.register_node("default:damier_black", {
+	description = "Black Damier Block",
+	tiles = {"default_damier_black.png"},
+	is_ground_content = false,
+	drop = {},
+	groups = {unbreakable=1},
+	sounds = default.node_sound_stone_defaults(),
+})
+
+minetest.register_node("default:damier_white", {
+	description = "White Damier Block",
+	tiles = {"default_damier_white.png"},
+	is_ground_content = false,
+	drop = {},
+	groups = {unbreakable=1},
+	sounds = default.node_sound_stone_defaults(),
+})
 
 
 local i
@@ -412,15 +473,6 @@ for _, i in pairs({"blue", "green", "orange"}) do
 		sounds = default.node_sound_stone_defaults(),
 	})
 end
-
-
-minetest.register_node("default:cloud", {
-	description = "Cloud",
-	tiles = {"default_cloud.png"},
-	sounds = default.node_sound_defaults(),
-	groups = {not_in_creative_inventory=1},
-})
-
 
 
 minetest.register_node("default:water_flowing", {
@@ -490,6 +542,7 @@ minetest.register_node("default:water_source", {
 	groups = {water=3, liquid=3, puts_out_fire=1, freezes=1},
 })
 
+
 minetest.register_node("default:water_source_static", {
 	description = "Static Water Source",
 	inventory_image = minetest.inventorycube("default_water.png"),
@@ -517,6 +570,7 @@ minetest.register_node("default:water_source_static", {
 	post_effect_color = {a=64, r=100, g=100, b=200},
 	groups = {static = 1, liquid=3, puts_out_fire=1, freezes=1},
 })
+
 
 --lava
 minetest.register_node("default:lava_source", {
@@ -552,6 +606,7 @@ minetest.register_node("default:lava_source", {
 	groups = {lava=3, liquid=2, hot=3, igniter=1},
 })
 
+
 minetest.register_node("default:lava_source_static", {
 	description = "Static Lava Source",
 	inventory_image = minetest.inventorycube("default_lava.png"),
@@ -585,6 +640,7 @@ minetest.register_node("default:lava_source_static", {
 	groups = {lava = 3, static = 1, liquid=2, hot = 2, igniter = 1},
 })
 
+
 -- invisible glass , player die when touch 
 minetest.register_node("default:block_die",{
 	description = "Invisible Die Block",
@@ -598,7 +654,4 @@ minetest.register_node("default:block_die",{
 	groups = {unbreakable=1},
 	damage_per_second = 20,
 })
-
-
-
 
